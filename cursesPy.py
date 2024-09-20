95cab126e1ae160dafdbeabf9d6fc265772e56f7
jordanphotis <jpho@udel.edu>
2024-09-20 14:43:13 -0400

    Added random number generators to insert text at random spots.

--- cursesPy.py
+++ cursesPy.py
@@ -1,4 +1,7 @@
 import curses
+import random
+
+
 
 # Initializes screen
 screen = curses.initscr()
@@ -14,7 +17,10 @@ while 1:
 	for j in ran:
 		for i in ran:
 			screen.addstr((i% 30 + j) % 43, j % 100, "This string gets printed at position (0, 0)")
-	
+			
+			if not i % 20:
+				screen.addstr(random.randrange(0,40),random.randrange(0,100), "#")	
+
 		screen.refresh()
 		curses.napms(50)		
 
